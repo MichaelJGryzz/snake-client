633f1675843c7e392899eb21e684ab18d3b55b65
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-19 23:47:01 -0400

    Add "error" event handler to "connect" function to handle potential connection errors.

--- play.js
+++ play.js
@@ -10,9 +10,15 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // "data" event handler handles incoming data and console.logs it for the player
   conn.on("data", (data) => {
     console.log("The server says: ", data);
-  })
+  });
+
+  // "error" event handler handles potential connection errors
+  conn.on('error', (error) => {
+    console.error('Connection error:', error);
+  });
 
   return conn;
 };
