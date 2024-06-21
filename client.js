4abd9d46588ddabeade361f523da767090ac7a09
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-20 22:36:38 -0400

    Create "client.js" file. Move "connect" function from "play.js" into this file along with code to require "net" library from Node. Add code to export "connect" function at the bottom of the file.

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // "data" event handler handles incoming data and console.logs it for the player
+  conn.on("data", (data) => {
+    console.log("The server says: ", data);
+  });
+
+  // "error" event handler handles potential connection errors
+  conn.on('error', (error) => {
+    console.error('Connection error:', error);
+  });
+
+  return conn;
+};
+
+// export "connect" function
+module.exports = connect;
