f815a3ed0bc482a02b2b0a0509eb8f83804e5ab3
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-24 02:21:35 -0400

    Create "constants.js" file to store constant values. Add "IP" and "PORT" constants for server connection. Add movement constants (MOVE_UP, MOVE_LEFT, MOVE_DOWN, MOVE_RIGHT). Create "MESSAGES" object to map key inputs to canned messages. Add code at the bottom of the file to export the constants and the object for other files to import and use.

---
+++ constants.js
@@ -0,0 +1,35 @@
+// constant for the IP address
+const IP = 'localhost';
+
+// constant for the PORT number
+const PORT = 50541;
+
+// constants for movement
+const MOVE_UP = "Move: Up";
+const MOVE_LEFT = "Move: Left";
+const MOVE_DOWN = "Move: Down";
+const MOVE_RIGHT = "Move: Right";
+
+// object to store canned messages
+const MESSAGES = {
+  1: "Say: Hello!",
+  2: "Say: Nice move!",
+  3: "Say: GG!",
+  4: "Say: Almost had you!",
+  5: "Say: I'm a snaaaake!",
+  6: "Say: Woohoo!",
+  7: "Say: Yay!!",
+  8: "Say: Yeehaw!",
+  9: "Say: Oh no!",
+  0: "Say: I win!",
+};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  MOVE_RIGHT,
+  MESSAGES,
+};
